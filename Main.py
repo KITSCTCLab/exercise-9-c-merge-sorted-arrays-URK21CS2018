af6d576aec39d1d7bc28881863ef06e04cc8781d
URK21CS2018 <109648374+URK21CS2018@users.noreply.github.com>
2022-10-11 20:50:27 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,13 @@
+from typing import List
+
+def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+  x = nums1[0:m]
+  y = nums2[0:n]
+  nums1 = x + y
+  nums1 = merge_sort(nums1)
+  return nums1
+  
+  
 def merge_sort(myList) -> None:
   if len(myList) > 1:
         mid = len(myList) // 2
